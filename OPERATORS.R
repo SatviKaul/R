5a75de5666f30fc9e01352099a41739e3038c311
Satvik Kaul <satvik.1799@gmail.com>
2019-04-07 09:15:15 +0530

    Create OPERATORS.R

---
+++ OPERATORS.R
@@ -0,0 +1,28 @@
+ #OPERATORS
+ x<-c(1.5,3.9)
+ y<-c(9.2,4)
+ print(x+y)
+ print(x-y)
+ print(x*y)
+ print(y/x)
+ print(x%%y)
+ print(x**y)
+ print(x**2)
+ print(x**3)
+ print(1*69)
+ print(23*3)
+
+ #RELATIONAL
+ x=10.4
+ y=8.6
+ print(x&y)
+ print(x&&y)
+ print(x||y)
+ print(x==y)
+ print(x!=y)
+
+# && compares just first element of each vector and returns true if true.
+#|| compares first two elements, like above.
+v<-2:20
+ print(v)
+ print(5 %in%v)
