a4de045b9fd106e357217d91b715c8d230477478
Satvik Kaul <satvik.1799@gmail.com>
2019-04-09 07:25:47 +0530

    Create LOOPS.R

---
+++ LOOPS.R
@@ -0,0 +1,30 @@
+ v<-c("hello","world")
+ c<-1
+ repeat{
++ print(v)
++ c<-c+1
++ if(c5){
++ break
++ }
++ }
+v<-c("hello","world")
+ c<-1
+ repeat{
++ }
+ v<-c("hello","world")
+ c<-1
+ while(c<10){
++ print(v)
++ c<-c+1
++ }
+v<-letters[1:10]
+ for(i in v){
++ print(i)
++ }
+ v<- LETTERS[1:6]
+ for (i in v){
++     if(i=='D'){
++         next
++     }
++     print(i)
++ }
